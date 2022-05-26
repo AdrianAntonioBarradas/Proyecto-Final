f03ad5327178864e307fdbcb960858debc6a8a26
AdrianAntonioBarradas <98973390+AdrianAntonioBarradas@users.noreply.github.com>
2022-05-26 13:22:30 -0500

    adaptado a react18.0.2

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App0 from "./App0";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App0 />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const container = document.getElementById("root");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App0 tab="home" />);
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App0 />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
