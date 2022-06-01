3724d09ec3d64ed945a6fcabd43184301a201a84
AdrianAntonioBarradas <420093774@pcpuma.acatlan.unam.mx>
2022-06-01 11:44:48 -0500

    mejorando login

--- src/vista/componentes/Login.js
+++ src/vista/componentes/Login.js
@@ -1,21 +1,23 @@
 import { useState } from "react";
-// import { useNavigate } from "react-router-dom";
 import   "../login/script.js"
 import "./login.css"
+
+import "../login/Siuu.wav";
 
 
 export default function Login() {
     // const navigate = useNavigate();
     const [id, setName] = useState("");
     const [pass, setPass] = useState("");
-  
+    
+    
     const handleSubmit = (event) => {
     try {
         event.preventDefault();
         // name== ? 
         var inputAccount = document.getElementById("account");
         var inputPass = document.getElementById("password");
-        if (id == "") { 
+        if (id === "") { 
           inputAccount.classList.remove("correct-input");
           inputAccount.classList.add("error-input");
         } else {
@@ -23,7 +25,7 @@ export default function Login() {
           inputAccount.classList.remove("error-input");
         }
 
-        if (pass == "") { 
+        if (pass === "") { 
           inputPass.classList.remove("correct-input");
           inputPass.classList.add("error-input");
         } else {
@@ -31,8 +33,10 @@ export default function Login() {
           inputPass.classList.remove("error-input");
         }
 
-        if (pass !="" && id!="" ){
-          console.log(inputAccount);
+        if (pass !=="" && id!="" ){
+          
+          
+          console.log(pass+" sdfsdf  "+id);
           // alert(`The id you entered was: ${id} sith la contra: ${pass}`);
           // navigate("/admin");
         }
@@ -43,30 +47,36 @@ export default function Login() {
       
     }
 
-    const userId=id;
+    var audio = new Audio("Siuu.wav");
+    document.onclick = function() {
+      audio.play();
+    }
+
+    
+
     
 
     return (
       <div className="container">
         <h1>Iniciar sesión</h1>
         <form onSubmit={handleSubmit} className="form-group">
-          <div class="form-check"> 
+          <div className="form-check"> 
             <label>Número de cuenta:</label>
               <input className="form-control" id="account" type="number"  value={id} onChange={(e) => setName(e.target.value)}/>
           </div>
-          <div class="form-check"> 
+          <div className="form-check"> 
           <label>Contraseña:</label>
             <input className="form-control" id="password" type="password"   value={pass}  onChange={(e) => setPass(e.target.value)} />
           </div>
           <div className="center">
           {/* aquí la parte prueba de los control btn */}
         
-        <div class="input-group mb-3">
+        <div className="input-group mb-3">
                     <div className="input-group-prepend">
-                      <label className="input-group-text" for="inputGroupSelect01">Rol:</label>
+                      <label className="input-group-text" htmlFor="inputGroupSelect01">Rol:</label>
                     </div>
                     <select className="custom-select" id="inputGroupSelect01">
-                      <option selected >...</option>
+                      <option  >...</option>
                       <option value={3}>Alumno</option>
                       <option value={2}>Profesor</option>
                       <option value={1}>Admin</option>
