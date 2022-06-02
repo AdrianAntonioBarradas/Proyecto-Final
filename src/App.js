915c9ddc665f039b3b7681c0c8715f86fdc4fccf
AdrianAntonioBarradas <420093774@pcpuma.acatlan.unam.mx>
2022-06-02 17:53:17 -0500

    si

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Header from "./vista/componentes/admin/Header";
 import Profesores from "./vista/componentes/admin/Profesores";
 import ManageAdmin from "./vista/componentes/admin/ManageAdmin";
 import Login from "./vista/componentes/Login";
-import NotFoundPage from "./vista/componentes/NotFoundPage";
+import ManageProfe from "./vista/componentes/Prof/ManageProfe";
 
 // store import
 // import Store from "./vista/componentes/Store";
@@ -30,7 +30,7 @@ function App() {
   return (
      state.id ? ({
         1: <ManageAdmin />,
-        2: "es uno",
+        2: <ManageProfe />,
         3: "es dos",
         null: <Login />
       }[state.role])
